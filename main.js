e5a061016c9e98346be939a73a34740267ec0b64
Daehyun <dhc4192@gmail.com>
2020-04-07 16:10:12 -0400

    api data received

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("working");
 
 const inputNumber = document.querySelector("inputNum");
-const selector = document.querySelector("currencySelector");
+const selector = document.querySelector("#currencySelector");
 const showResult = document.querySelector("result");
 const convert = document.querySelector("button");
 const additionalInfo = document.querySelector("addInfo");
@@ -10,15 +10,23 @@ const BASE_URL =
   "https://prime.exchangerate-api.com/v5/ea19256df9d432e21c8cfc0e/latest/USD";
 
 function addRates(response) {
-  const conversionRates = response.data.conversion_rates
-  const queryOptions = document.querySelectorAll('option')
-  conversionRates.forEach(function () {
-    queryOptions.appendChild()
-  })
+  const conversionRates = response.data.conversion_rates;
+  for (keys in conversionRates) {
+    let option = document.createElement('option')
+    option.text = keys
+    option.value = conversionRates[keys]
+    selector.add(option, null)
+  }
 }
+
+convert.addEventListener('click', () => {
+  console.log(selector.options[selector.selectedIndex].value)
+})
+
 
 async function getRates() {
   const response = await axios.get(BASE_URL);
-  addRates(response)
+  console.log(response)
+  addRates(response);
 }
-getRates()
+getRates();
