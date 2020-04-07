fb5580e342c730e16bef9cde2a8e994ffb6bc9ff
Daehyun <dhc4192@gmail.com>
2020-04-07 13:35:00 -0400

    first js

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-console.log("working")
+console.log("working");
 
-const inputNumber = document.querySelector('inputNum')
-const selector = document.querySelector('currencySelector')
-const showResult = document.querySelector('result')
-const convert = document.querySelector('button')
-const additionalInfo = document.querySelector('addInfo')
+const inputNumber = document.querySelector("inputNum");
+const selector = document.querySelector("currencySelector");
+const showResult = document.querySelector("result");
+const convert = document.querySelector("button");
+const additionalInfo = document.querySelector("addInfo");
 
-const BASE_URL = 'https://prime.exchangerate-api.com/v5/ea19256df9d432e21c8cfc0e/latest/USD'
+const BASE_URL =
+  "https://prime.exchangerate-api.com/v5/ea19256df9d432e21c8cfc0e/latest/USD";
 
-
-
-
-
-
-
-
-
-
+function addRates(response) {
+  const conversionRates = response.data.conversion_rates
+  const queryOptions = document.querySelectorAll('option')
+  conversionRates.forEach(function () {
+    queryOptions.appendChild()
+  })
+}
 
 async function getRates() {
-  const response = await axios.get(BASE_URL)
+  const response = await axios.get(BASE_URL);
+  addRates(response)
 }
 getRates()
