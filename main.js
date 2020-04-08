09615f8908de7d193bc0c68b56454bb29ee1abca
Daehyun <dhc4192@gmail.com>
2020-04-07 21:44:19 -0400

    updated

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-console.log("working");
+// console.log("working");
 
-const selector = document.querySelector("#currencySelector");
-const showResult = document.querySelector("#result");
-const convert = document.querySelector("button");
-const additionalInfo = document.querySelector("#addInfo");
+// const selector = document.querySelector("#currencySelector");
+// const showResult = document.querySelector("#result");
+// const convert = document.querySelector("button");
+// const additionalInfo = document.querySelector("#addInfo");
 
-const BASE_URL =
-  "https://prime.exchangerate-api.com/v5/ea19256df9d432e21c8cfc0e/latest/USD";
+// const BASE_URL =
+//   "https://prime.exchangerate-api.com/v5/ea19256df9d432e21c8cfc0e/latest/USD";
 
-function addRates(response) {
-  const conversionRates = response.data.conversion_rates;
-  for (keys in conversionRates) {
-    let option = document.createElement('option')
-    option.text = keys
-    option.value = conversionRates[keys]
-    selector.add(option, null)
-  }
-}
+// function addRates(response) {
+//   const conversionRates = response.data.conversion_rates;
+//   for (keys in conversionRates) {
+//     let option = document.createElement('option')
+//     option.text = keys
+//     option.value = conversionRates[keys]
+//     selector.add(option, null)
+//   }
+// }
 
-async function getRates() {
-  const response = await axios.get(BASE_URL);
-  console.log(response)
-  addRates(response);
-}
-getRates();
+// async function getRates() {
+//   const response = await axios.get(BASE_URL);
+//   console.log(response)
+//   addRates(response);
+// }
+// getRates();
 
-convert.addEventListener('click', () => {
-  const inputNumber = document.getElementById("inputNum").value
-  const internationalRates = selector.options[selector.selectedIndex].value
-  const conversion = inputNumber * internationalRates
+// convert.addEventListener('click', () => {
+//   const inputNumber = document.getElementById("inputNum").value
+//   const internationalRates = selector.options[selector.selectedIndex].value
+//   const conversion = inputNumber * internationalRates
 
-  showResult.value = conversion
-})
+//   showResult.value = conversion
+// })
